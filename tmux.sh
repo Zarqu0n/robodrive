03e693eff1e534a2784de9712effde24d950fe7c
Zarqu0n <zarquon.875@gmail.com>
2023-11-12 20:17:50 +0300

    add twist_mux launch terminal

--- tmux.sh
+++ tmux.sh
@@ -13,22 +13,17 @@ tmux new-session -d -s $session -n base
 
 tmux send-keys "export GAZEBO_MODEL_PATH=robodrive_simulation/models;source ../../devel/setup.bash;roslaunch robodrive_simulation sim.launch" C-m
 
+
 tmux selectp -t 0
 tmux splitw -h 
 tmux send-keys "sleep 3;source ../../devel/setup.bash;roslaunch robodrive_navigation navigation.launch namespace:=robodrive0 open_rviz:=true" C-m
 
 tmux selectp -t 1
 tmux splitw
-tmux send-keys "sleep 3;source ../../devel/setup.bash;roslaunch robodrive_simulation robot_spawner.launch namespace:=robodrive0 x_pos:=2" C-m
+tmux send-keys "sleep 3;source ../../devel/setup.bash;roslaunch robodrive_simulation robot_spawner.launch namespace:=robodrive0" C-m
 
-tmux selectp -t 0
-tmux splitw -h
-tmux send-keys "sleep 3;source ../../devel/setup.bash;roslaunch robodrive_navigation navigation.launch namespace:=robodrive1 open_rviz:=false" C-m
-
-
-tmux selectp -t 1
+tmux selectp -t 2
 tmux splitw
-tmux send-keys "sleep 3;source ../../devel/setup.bash;roslaunch robodrive_simulation robot_spawner.launch namespace:=robodrive1"  C-m
-
+tmux send-keys "sleep 6;source ../../devel/setup.bash;roslaunch robodrive_controller twist_mux.launch" C-m
 
 tmux a
